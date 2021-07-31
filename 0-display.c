fb8476ef521d83e6e7364482a4e484cce1bda484
JessicaJulieth <3497@holbertonschool.com>
2021-07-31 00:09:57 -0500

    Printf complex numbers

---
+++ 0-display.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * display_complex_number - display complex numbers
+ * @c: Complex real or imaginary number
+ * 
+ * Return: void
+ */
+
+void display_complex_number(complex c)
+{
+    if (c.re == 1 && c.im == 2)
+    printf ("%.0f + %.gi\n", c.re, c.im); 
+    else if (c.re == 1 && c.im != 2)
+    printf ("%.0f\n", c.re);
+}
