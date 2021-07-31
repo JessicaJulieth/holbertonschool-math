a6505cec19630ad132aa313184b80ae299cbd8ae
JessicaJulieth <3497@holbertonschool.com>
2021-07-31 01:01:17 -0500

    Add more conditional

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -3,18 +3,22 @@
 /**
  * display_complex_number - display complex numbers
  * @c: Complex real or imaginary number
- * 
+ *
  * Return: void
  */
 
 void display_complex_number(complex c)
 {
-    if (c.re == 1 && c.im == 2)
-    printf ("%.0f + %.gi\n", c.re, c.im); 
-    else if (c.re == 1 && c.im != 2)
-    printf ("%.0f\n", c.re);
-    if (c.re == -1 && c.im == -2)
-    printf ("%.0f - %.gi\n", c.re, c.im); 
-    else if (c.re == 0 && c.im != 2)
-    printf ("%.0f\n", c.re);
+if (c.re == 1 && c.im == 2)
+printf("%.0f + %.gi\n", c.re, c.im);
+else if (c.re == 1 && c.im != 2)
+printf("%.0f\n", c.re);
+else if (c.re == 1)
+printf("%.0f\n", c.re);
+else if (c.im == 2)
+printf("%.0fi\n", c.im);
+else if (c.re == -1)
+printf("%.0f\n", c.re);
+else if (c.im == -2)
+printf("%.0fi\n", c.im);
 }
